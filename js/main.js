10c258004b3900568881aa0278b5fadddce70090
Suguru13 <91862433+Suguru13@users.noreply.github.com>
2021-11-02 20:30:59 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+'use strict'; 
+
+{
+    // IntersectionObserver API
+    
+    function callback(entries, obs) { 
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) { 
+                return;
+            }
+
+            entry.target.classList.add('appear');
+            obs.unobserve(entry.target);
+        });
+    }
+
+    const options = { 
+        threshold: 0.2,
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    const targets = document.querySelectorAll('.videoblock');
+
+    targets.forEach(target => {
+        observer.observe(target);
+    });
+
+
+
+}
